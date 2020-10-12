05f9c8668d00e573313c22ef7996e01a9b9dad9f
Nathan Polomark <polomarkn@hotmail.com>
2020-10-11 17:03:37 -0700

    Wrote questions

--- index.js
+++ index.js
@@ -1,6 +1,50 @@
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project title?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "What is the description for your project?"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What steps are needed to install your project?"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "How do you use your project?"
+    },
+    {
+        type: "input",
+        name: "license",
+        message: "What license is your project under?"
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "Who contributed to this project?"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Write a test for other users here:"
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "What is your project title?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email?"
+    }
 ];
 
 // function to write README file
